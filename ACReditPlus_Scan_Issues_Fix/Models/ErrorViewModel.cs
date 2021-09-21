2710bd3c43e7a277e66091625db5b67d03c8c250
kwang <kwang@acr.org>
2021-09-21 14:35:17 -0400

    Created a ASP.NET Core 3.1 MVC app.

---
+++ ACReditPlus_Scan_Issues_Fix/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ACReditPlus_Scan_Issues_Fix.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
