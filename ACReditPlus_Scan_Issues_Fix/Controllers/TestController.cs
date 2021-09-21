bcdff002a40f7bafb0dfc81bc134ad7a94fb6c0d
kwang <kwang@acr.org>
2021-09-21 15:07:14 -0400

    Added a post method to reproduce the "Reflected_XSS_All_Clients" issue.

---
+++ ACReditPlus_Scan_Issues_Fix/Controllers/TestController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ACReditPlus_Scan_Issues_Fix.Controllers
+{
+	public class TestClass
+	{
+		public string Name { get; set; }
+	}
+
+	public class TestController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public ActionResult FilteredReviewerFeedbackList([FromBody] TestClass searchModel)
+		{
+			return Json(searchModel);
+		}
+	}
+}
