f2cba6c120f4155a6b8faec36993a9b338ebf325
kwang <kwang@acr.org>
2022-03-08 10:28:49 -0500

    Check "stored path traversal" issue.

---
+++ ACReditPlus_Scan_Issues_Fix/Controllers/JacketLabelController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ACReditPlus_Scan_Issues_Fix.Controllers
+{
+	public class JacketLabelController : Controller
+	{
+        public ActionResult ConstructJacketLabelPDF(string examHasFormDocId)
+        {
+            try
+            {
+                // Generate the File and Get the Path.
+                string fileFullPath = this.jacketLabelWorklistHelper.GetJacketLabelPDFFileByteInfo(examHasFormDocId);
+                // Read The File Bytes
+                byte[] fileBytes = System.IO.File.ReadAllBytes(fileFullPath);
+                // Get the File Info from The Path 
+                System.IO.FileInfo fileInfo = new System.IO.FileInfo(fileFullPath);
+                // Return the File Bytes with File Name so it can be Downloaded.
+                return File(fileBytes, "application/pdf", fileInfo.Name);
+            }
+            catch (Exception e)
+            {
+                HttpContextHelper.HttpContext.RiseError(e);
+                throw e;
+            }
+
+        }
+    }
+}
