57bd6a3a1981a29560b8e37dd0768665789e6662
kwang <kwang@acr.org>
2022-03-08 10:45:37 -0500

    Added regular expression check to path.

--- ACReditPlus_Scan_Issues_Fix/Controllers/JacketLabelController.cs
+++ ACReditPlus_Scan_Issues_Fix/Controllers/JacketLabelController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ACReditPlus_Scan_Issues_Fix.Helpers;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ACReditPlus_Scan_Issues_Fix.Controllers
@@ -15,7 +16,7 @@ namespace ACReditPlus_Scan_Issues_Fix.Controllers
                 // Generate the File and Get the Path.
                 string fileFullPath = this.jacketLabelWorklistHelper.GetJacketLabelPDFFileByteInfo(examHasFormDocId);
                 // Read The File Bytes
-                byte[] fileBytes = System.IO.File.ReadAllBytes(fileFullPath);
+                byte[] fileBytes = System.IO.File.ReadAllBytes(fileFullPath.GetValidFilename());
                 // Get the File Info from The Path 
                 System.IO.FileInfo fileInfo = new System.IO.FileInfo(fileFullPath);
                 // Return the File Bytes with File Name so it can be Downloaded.
--- ACReditPlus_Scan_Issues_Fix/Helpers/ExtensionMethods.cs
+++ ACReditPlus_Scan_Issues_Fix/Helpers/ExtensionMethods.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace ACReditPlus_Scan_Issues_Fix.Helpers
@@ -10,6 +11,16 @@ namespace ACReditPlus_Scan_Issues_Fix.Helpers
 		public static string DbConnStringEnableColumnEncryption(this string dbConnectionString)
 		{
 			return $"{dbConnectionString} Column Encryption Setting=enabled;";
+		}
+
+		public static string GetValidFilename(this string filename)
+		{
+			Regex rg = new Regex("^[A-Za-z0-9#_.-]+$");
+			if (String.IsNullOrEmpty(filename) || !rg.IsMatch(filename))
+			{
+				throw new Exception($"Filename '{filename}' is invalid.");
+			}
+			return filename;
 		}
 	}
 }
