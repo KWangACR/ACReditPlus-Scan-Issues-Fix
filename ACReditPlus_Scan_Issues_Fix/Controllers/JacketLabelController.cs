f38d3d6065cd3a9886c62e34b58adb8defbc85b1
kwang <kwang@acr.org>
2022-03-08 10:59:20 -0500

    Separate folder and file path.

--- ACReditPlus_Scan_Issues_Fix/Controllers/JacketLabelController.cs
+++ ACReditPlus_Scan_Issues_Fix/Controllers/JacketLabelController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using ACReditPlus_Scan_Issues_Fix.Helpers;
@@ -16,7 +17,8 @@ namespace ACReditPlus_Scan_Issues_Fix.Controllers
                 // Generate the File and Get the Path.
                 string fileFullPath = this.jacketLabelWorklistHelper.GetJacketLabelPDFFileByteInfo(examHasFormDocId);
                 // Read The File Bytes
-                byte[] fileBytes = System.IO.File.ReadAllBytes(fileFullPath.GetValidFilename());
+                //byte[] fileBytes = System.IO.File.ReadAllBytes(fileFullPath.GetValidFilename());
+                byte[] fileBytes = System.IO.File.ReadAllBytes(Path.Combine("userFiles", Path.GetFileName(fileFullPath)));
                 // Get the File Info from The Path 
                 System.IO.FileInfo fileInfo = new System.IO.FileInfo(fileFullPath);
                 // Return the File Bytes with File Name so it can be Downloaded.
