e15303eb698877bd2c341fb69cab85481815b801
kwang <kwang@acr.org>
2022-03-09 09:42:22 -0500

    Checked "StreamWriter".

---
+++ ACReditPlus_Scan_Issues_Fix/Helpers/BaseDomainServiceManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ACReditPlus_Scan_Issues_Fix.Helpers
+{
+	public class BaseDomainServiceManager
+	{
+	}
+}
---
+++ ACReditPlus_Scan_Issues_Fix/Helpers/SecureTokenHelper.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACReditPlus_Scan_Issues_Fix.Helpers
+{
+    public class SecureTokenHelper
+    {
+
+        //private readonly static string keyPayFlowSectionName = "externalFlow/payflowSettings";
+        private readonly static string keyHostAddress = "HostAddress";
+        private readonly static string keyPartner = "PARTNER";
+        private readonly static string keyVendor = "VENDOR";
+        private readonly static string keyUser = "USER";
+        private readonly static string keyPsd = "PWD";
+        private readonly static string keyTrxType = "TrxType";
+        private readonly static string keyCheckoutPage = "CheckoutUrl";
+        private readonly static string keyMode = "Mode";
+
+        public SecureTokenRequestInfo SetSecurityToken(PayPalCheckout payPalCheckout)
+        {
+
+            SecureTokenRequestInfo requestData = new SecureTokenRequestInfo();
+
+            try
+            {
+                requestData = GetSecureTokenRequestByExistedData(Guid.NewGuid().ToString(), payPalCheckout);
+                string postUrl = requestData.RequestUrl;
+                HttpWebRequest req = (HttpWebRequest)WebRequest.Create(@"https://" + postUrl);
+
+                //Set values for the request back
+                req.Method = "POST";
+                req.ContentType = "application/x-www-form-urlencoded";
+                string secureTokenRequestContent = ParserRequestContent(requestData);
+                string strRequest = secureTokenRequestContent;
+                req.ContentLength = strRequest.Length;
+
+                //for proxy
+                //WebProxy proxy = new WebProxy(new Uri("http://url:port#"));
+                //req.Proxy = proxy;
+                //Send the request to PayPal and get the response
+                StreamWriter streamOut = new StreamWriter(req.GetRequestStream(),
+                    Encoding.ASCII);
+                streamOut.Write(strRequest);
+                streamOut.Close();
+                StreamReader streamIn = new StreamReader(req.GetResponse().GetResponseStream());
+                string strResponse = streamIn.ReadToEnd();
+                streamIn.Close();
+            }
+            catch (Exception exc)
+            {
+                //add exception handle
+                requestData.RequestException = exc;
+            }
+
+            return requestData;
+        }
+    }
+}
