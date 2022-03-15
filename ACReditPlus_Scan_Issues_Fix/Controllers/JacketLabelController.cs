847e7abbdedc4c52aafbee6c8d34a4277bcd03c5
kwang <kwang@acr.org>
2022-03-15 14:06:09 -0400

    Added "ReadAllBytes".

--- ACReditPlus_Scan_Issues_Fix/Controllers/JacketLabelController.cs
+++ ACReditPlus_Scan_Issues_Fix/Controllers/JacketLabelController.cs
@@ -15,7 +15,7 @@ namespace ACReditPlus_Scan_Issues_Fix.Controllers
             try
             {
                 // Read The File Bytes
-                byte[] fileBytes = System.IO.File.ReadAllBytes(Path.Combine("userFiles", "filePath.txt"));
+                byte[] fileBytes = System.IO.File.ReadAllBytes(@"C:\acreditplus_DEV\Application\Source");
                 // Return the File Bytes with File Name so it can be Downloaded.
                 return File(fileBytes, "application/pdf", "fileName");
             }
